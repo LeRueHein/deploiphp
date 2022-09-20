12bf77a7b81f711f3ff773c082f94f34a9464071
Faxiis <antho.lavoute@gmail.com>
2022-09-20 11:51:49 +0200

    correction heure

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
 <?php
-echo date('Y-m-d H:i:s');  // 2012-10-11 15:35:53
+
 echo date('l j F Y, H:i'); // Thursday 11 October 2012, 15:35
-echo date('d F Y');        // 11 October 2012
-echo date('d/m/Y');        // 11/10/12
-?>
